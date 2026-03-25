fe4272e0f35f5d8027cbc91290c0aa126415728d
jigyasa-sadh <jigyasasadh24@gmail.com>
2026-03-25 17:17:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function searchPlaces() {
+
+let input = document.getElementById("searchInput").value.toLowerCase();
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+let text = card.innerText.toLowerCase();
+
+if(text.includes(input)){
+card.style.display = "block";
+}
+else{
+card.style.display = "none";
+}
+
+});
+
+}
